ba8e4c50e2ae8c3eb93ae5a4c335c36ddb3d5a0d
daniel springer <daniel.springer@gmx.at>
2024-01-03 14:05:04 +0100

    Minor.

--- code/train.py
+++ code/train.py
@@ -8,31 +8,31 @@ from pytorch_lightning.loggers import TensorBoardLogger
 def train():
     config = {}
     config["MODEL_NAME"] = "GreenGNN"
-    config["PATH_TRAIN"] = "../data/batch1.hdf5"
+    config["PATH_TRAIN"] = "../data/batch1_UGrid.hdf5"
     config["batch_size"] = 1
     config["learning_rate"] = 1e-4
-    config["weight_decay"] = 0
+    config["weight_decay"] = 1e-5
 
     data_set = load_data.Dataloader_graph(config)
-    train1_set, validation_set = torch.utils.data.random_split(data_set, [int(data_set.__len__()*0.8), int(data_set.__len__()*0.2)], generator=torch.Generator().manual_seed(42))
-    train_dataloader = DataLoader(train1_set, batch_size=config["batch_size"], shuffle=True)
+    train_set, validation_set, unused_set = torch.utils.data.random_split(data_set, [int(data_set.__len__()*0.3), int(data_set.__len__()*0.05), int(data_set.__len__()*0.65)], generator=torch.Generator().manual_seed(42))
+    train_dataloader = DataLoader(train_set, batch_size=config["batch_size"], shuffle=True)
     validation_dataloader = DataLoader(validation_set, batch_size=config["batch_size"], shuffle=True)
 
     model = models.model_wraper_gnn(config)
-    # SAVEPATH = "/gpfs/data/fs72150/springerd/Projects/LuttingerWard_from_ML/saves/save_GreenGNN_2023-12-17/version_2/checkpoints/epoch=16-step=340000.ckpt"
-    # checkpoint = torch.load(PATH)#, map_location=torch.device('cpu'))
-    # model.load_state_dict(checkpoint['state_dict'])
-    # print("...apparently loaded model.")
+    SAVEPATH = "../saves/UGrid/save_GreenGNN_2023-12-19/epoch_19/checkpoints/epoch=13-step=2100000.ckpt"
+    checkpoint = torch.load(SAVEPATH)#, map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint['state_dict'])
+    print("...apparently loaded model.")
 
     PATH = ""
-    CONFIGURATION = f"../saves/save_{config['MODEL_NAME']}_{datetime.datetime.now().date()}"
+    CONFIGURATION = f"../saves/UGrid/save_{config['MODEL_NAME']}_{datetime.datetime.now().date()}"
     logger = TensorBoardLogger(PATH, name=CONFIGURATION)
 
     import pytorch_lightning as pl
     from pytorch_lightning.plugins.environments import LightningEnvironment
 
     # ### SLURM Training
-    trainer = pl.Trainer(max_epochs=20, accelerator='gpu', devices=2, num_nodes=4, strategy='ddp', logger=logger)
+    trainer = pl.Trainer(max_epochs=11, accelerator='gpu', devices=2, num_nodes=1, strategy='ddp', logger=logger)
     # ### Jupyter Notebook Training
     # # trainer = pl.Trainer(max_epochs=20, accelerator='gpu', devices=1, strategy='auto', logger=logger, plugins=[LightningEnvironment()])
 
